acdd4e875092e76352b21d581a7ef7352a34929f
Matheus <101599970+matheusrails@users.noreply.github.com>
2025-04-15 14:01:40 +0000

    add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # simple-news
+
 implementation of https://www.tabnews.com.br/ for https://curso.dev/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>simple news</h1>
+  return <h1>simple news</h1>;
 }
 
 export default Home;
